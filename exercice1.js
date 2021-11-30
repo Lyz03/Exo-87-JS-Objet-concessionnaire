a1f70ff9271bf964d0629b0ad619888990c47371
Lyz03 <lizoe@netcourrier.com>
2021-11-30 12:05:10 +0100

    exo 87

--- exercice1.js
+++ exercice1.js
@@ -1,15 +1,19 @@
-let voiture = {
-    "Nom" : "Eggo",
-    "imgUrl" : "https://images.caradisiac.com/logos/9/9/8/2/159982/S7-Citroen-Eggo-concept-de-citadine-du-futur-73689.jpg",
-    "nombresRoues" : 4,
-    "Couleur" : "Marron",
-    "Contructeur" : "Citroen",
-    "Carburant" : "Solaire",
-    "nombrePortes" : 3,
-    "Autonomie" : "450km",
-    "vitesseMaxi" : "120km/h"
+let Voiture = {
+    nom : "Eggo",
+    imgUrl : "https://images.caradisiac.com/logos/9/9/8/2/159982/S7-Citroen-Eggo-concept-de-citadine-du-futur-73689.jpg",
+    nombresRoues : 4,
+    couleur : "Marron",
+    constructeur : "Citroen",
+    carburant : "Solaire",
+    nombrePortes : 3,
+    autonomie : "450km",
+    vitesseMaxi : "120km/h"
 };
 
+document.querySelector('img').src = Voiture.imgUrl;
+document.getElementById('container').innerText = `Voici la voiture ${Voiture.nom}, elle possède ${Voiture.nombresRoues} roues et est de couleur ${Voiture.couleur}. 
+    \n Elle est fabriquée par ${Voiture.constructeur} et roule à l'énergie ${Voiture.carburant}. 
+    \n Elle possède ${Voiture.nombrePortes} portes, à une autonomie de ${Voiture.autonomie} et peut aller jusqu'à ${Voiture.vitesseMaxi}`;
 
 
 
